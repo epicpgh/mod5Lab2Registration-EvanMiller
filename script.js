23b72e67533f7b43a0cc362ecdd9239cccc23686
Evan Miller <ewmst5@gmail.com>
2025-05-19 15:06:21 -0400

    styling and javascript with help from the group and webdocs.

--- script.js
+++ script.js
@@ -5,36 +5,76 @@ const password = document.getElementById("password");
 const passwordError = document.getElementById("passwordError");
 const confirmPassword = document.getElementById("confirmPassword");
 const confirmError = document.getElementById("confirmPasswordError");
+const usernameError =document.getElementById("usernameError");
+const emailError = document.getElementById("emailError");
 
 
 
-
-
-function addUsername(){
-    if (inputField.value === ''){
-        alert("Must enter a valid username, Homie");
+function validateUsername() {
+    if (username.validity.valueMissing) {
+      usernameError.textContent = "Username is required.";
+    } else if (username.validity.tooShort) {
+      usernameError.textContent = "Username must be at least 3 characters.";
+    } else {
+      usernameError.textContent = "";
     }
-    else {
-        let 
-    }
-
-}
-
-
-
-function addTask(){
-    if (inputField.value === '') {
-      alert("Don't forget to add a task.");
-    }
-    else{
-      let li = document.createElement("li");
-      li.innerHTML = inputField.value;
-      listContainer.appendChild(li);
-      let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }
-    
-    inputField.value = "";
-    saveInfo();
   }
+
+  function validateEmail(){
+    if (email.validity.valueMissing){
+        emailError.textContent = "email address is required.";
+     }else if (email.validity.typeMismatch){
+        emailError.textContent = "Please enter a valid email address. ex: j123@mail.com "
+
+     } else{
+        emailError.textContent ="";
+     }
+     }
+
+     function validatePassword() {
+        if (password.validity.valueMissing){
+            passwordError.textContent = "Password is required to proceed.";
+        }
+        else if (password.validity.tooShort) {
+          passwordError.textContent = "Password must be a minimum of 8 characters.";
+        } else if (password.validity.patternMismatch) {
+          passwordError.textContent = "Password must include an uppercase letter, a lowercase letter, and a number.";
+        } else {
+          passwordError.textContent = "";
+        }
+
+      }
+      function validateConfirmPassword() {
+        if (confirmPassword.validity.valueMissing) {
+          confirmError.textContent = "Please confirm your password.";
+        } else if (confirmPassword.value !== password.value) {
+          confirmError.textContent = "Passwords do not match.";
+        } else {
+          confirmError.textContent = "";
+        }
+      }
+
+      username.addEventListener("input", validateUsername);
+      email.addEventListener("input", validateEmail);
+      password.addEventListener("input", validatePassword);
+      confirmPassword.addEventListener("input", validateConfirmPassword);
+
+     
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    validateUsername();
+    validateEmail();
+    validatePassword();
+    validateConfirmPassword();
+
+    localStorage.setItem("username", username.value);
+
+    localStorage.setItem("email", email.value);
+  })
+
+
+
+
+ 
